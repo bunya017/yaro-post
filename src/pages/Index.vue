442ed7e665dbf6f21a71f60534a76336446b54db
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2020-02-10 13:21:38 +0100

    feat: Add request URL input

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,7 +1,16 @@
 <template>
   <q-page padding>
-    <div class="q-pa-md" style="max-width: 250px">
-      <q-select outlined v-model="requestType" :options="options" label="Request Type" />
+    <div class="row q-pa-md">
+      <div class="col-3" style="min-width: 250px">
+        <q-select
+          outlined v-model="requestType"
+          :options="options"
+          label="Request Type"
+        />
+      </div>
+      <div class="col-7 q-px-md">
+        <q-input outlined v-model="requestURL" label="Request URL" />
+      </div>
     </div>
   </q-page>
 </template>
@@ -12,6 +21,7 @@ export default {
   data () {
     return {
       requestType: null,
+      requestURL: 'https://',
       options: [
         {
           label: 'GET',
