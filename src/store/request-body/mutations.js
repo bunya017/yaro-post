ed0c85e1f6d9c255da7d2b683094eca9fa8153cd
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2020-02-26 14:07:28 +0100

    refactor(refactor): Add addRequestParameter mutation

--- src/store/request-body/mutations.js
+++ src/store/request-body/mutations.js
@@ -1,2 +1,11 @@
-export function someMutation (/* state */) {
+import Vue from 'vue'
+
+export function addRequestParameter (state) {
+  let paramCount = Object.keys(state.requestParams).length + 1
+  // Add 1 to paramCount so key turns out to be parameter1, parameter2 ...
+  Vue.set(state.requestParams, 'parameter' + paramCount, {
+    name: '',
+    value: '',
+    index: paramCount
+  })
 }
