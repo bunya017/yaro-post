fcf49df93634fc69dd08e06c662faaa50f2bd6d9
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2020-03-06 11:24:56 +0100

    refactor(store): Add type to requestParam object

--- src/store/request-body/mutations.js
+++ src/store/request-body/mutations.js
@@ -1,11 +1,12 @@
 import Vue from 'vue'
 
-export function addRequestParameter (state) {
+export function addRequestParameter (state, type) {
   let paramCount = Object.keys(state.requestParams).length + 1
   // Add 1 to paramCount so key turns out to be parameter1, parameter2 ...
   Vue.set(state.requestParams, 'parameter' + paramCount, {
     name: '',
     value: '',
+    type: type,
     index: paramCount
   })
 }
