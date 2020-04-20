41ce5ac83fa374dd1ab5abafc4589b3cd90d08ca
Bunyaminu Sani <bunyaminu007@hotmail.co.uk>
2020-04-20 01:30:19 +0100

    refactor(Index): Wrap Request and Request Body sections in QCard

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,108 +1,120 @@
 <template>
   <q-page padding>
-    <div class="bg-grey-3 rounded-borders">
-      <!-- Request Section-->
-      <div class="text-h6 q-px-md q-pt-md">Request</div>
-      <div class="row q-pa-md">
-        <div class="col-3">
-          <q-select
-            dense
-            outlined
-            label="Method"
-            :options="options"
-            v-model="requestMethod"
-          />
+    <!-- Request Section-->
+    <q-card>
+      <q-card-section class="q-py-sm">
+        <div class="text-h6">Request</div>
+      </q-card-section>
+      <q-separator />
+      <q-card-section>
+        <div class="row q-pa-md">
+          <div class="col-3">
+            <q-select
+              dense
+              outlined
+              label="Method"
+              :options="options"
+              v-model="requestMethod"
+            />
+          </div>
+          <div class="col-8 q-px-md">
+            <q-input dense outlined v-model="requestURL" label="URL" />
+          </div>
+          <div class="col-1">
+            <q-btn color="primary" label="Send" @click="sendRequest" />
+          </div>
         </div>
-        <div class="col-8 q-px-md">
-          <q-input dense outlined v-model="requestURL" label="URL" />
-        </div>
-        <div class="col-1">
-          <q-btn color="primary" label="Send" @click="sendRequest" />
-        </div>
-      </div>
-    </div>
-    <div
-      class="bg-grey-3 q-mt-md rounded-borders"
+      </q-card-section>
+    </q-card>
+
+    <!-- Request Body Section-->
+    <q-card
+      class="q-mt-md"
       v-if="(requestMethod.value !== 'get') && (requestMethod.value !== 'delete')"
     >
-      <!-- Request Body Section-->
-      <div class="text-h6 q-px-md q-pt-md">Request Body</div>
-      <div
-        :key="field.index"
-        v-for="field in requestParams"
-        class="row q-pa-md q-gutter-md"
-      >
-        <div class="col-5">
-          <q-input
-            dense
-            outlined
-            type="text"
-            placeholder="parameter name"
-            @change="
-              $store.commit('request/setRequestParameter', {
-                field: 'name',
-                index: field.index,
-                param: $event.target.value
-              })
-            "
-            :value="requestParams['parameter' + field.index].name"
-          />
+      <q-card-section class="q-py-sm">
+        <div class="text-h6">Request Body</div>
+      </q-card-section>
+      <q-separator />
+      <q-card-section class="q-py-sm">
+        <div
+          :key="field.index"
+          v-for="field in requestParams"
+          class="row q-pa-md q-gutter-md"
+        >
+          <div class="col-5">
+            <q-input
+              dense
+              outlined
+              type="text"
+              placeholder="parameter name"
+              @change="
+                $store.commit('request/setRequestParameter', {
+                  field: 'name',
+                  index: field.index,
+                  param: $event.target.value
+                })
+              "
+              :value="requestParams['parameter' + field.index].name"
+            />
+          </div>
+          <div class="col-5">
+            <q-input
+              dense
+              outlined
+              :type="field.type"
+              placeholder="value"
+              @change="
+                $store.commit('request/setRequestParameter', {
+                  field: 'value',
+                  index: field.index,
+                  param: $event.target.value
+                })
+              "
+              :value="requestParams['parameter' + field.index].value"
+            />
+          </div>
+          <div class="col-1">
+            <!-- Remove parameter button -->
+            <q-btn
+              dense
+              color="grey"
+              icon="delete"
+              @click="removeParameter(field.index)"
+            />
+          </div>
         </div>
-        <div class="col-5">
-          <q-input
-            dense
-            outlined
-            :type="field.type"
-            placeholder="value"
-            @change="
-              $store.commit('request/setRequestParameter', {
-                field: 'value',
-                index: field.index,
-                param: $event.target.value
-              })
-            "
-            :value="requestParams['parameter' + field.index].value"
-          />
+        <div class="row q-pa-md">
+          <div class="col-3 q-mx-auto">
+            <!-- New parameter button -->
+            <q-btn color="white" text-color="primary" icon="add" label="New parameter">
+              <q-menu fit>
+                <q-list>
+                  <q-item
+                    clickable
+                    v-close-popup
+                    @click="addParameter({ index: parameters.length + 1, type: 'text' })"
+                  >
+                    <q-item-section>Text</q-item-section>
+                  </q-item>
+                  <q-item
+                    clickable
+                    v-close-popup
+                    @click="addParameter({ index: parameters.length + 1, type: 'file' })"
+                  >
+                    <q-item-section>File</q-item-section>
+                  </q-item>
+                </q-list>
+              </q-menu>
+            </q-btn>
+          </div>
         </div>
-        <div class="col-1">
-          <!-- Remove parameter button -->
-          <q-btn
-            dense
-            color="grey"
-            icon="delete"
-            @click="removeParameter(field.index)"
-          />
-        </div>
-      </div>
-      <div class="row q-pa-md">
-        <div class="col-3 q-mx-auto">
-          <!-- New parameter button -->
-          <q-btn color="grey" icon="add" label="New parameter">
-            <q-menu fit>
-              <q-list>
-                <q-item
-                  clickable
-                  v-close-popup
-                  @click="addParameter({ index: parameters.length + 1, type: 'text' })"
-                >
-                  <q-item-section>Text</q-item-section>
-                </q-item>
-                <q-item
-                  clickable
-                  v-close-popup
-                  @click="addParameter({ index: parameters.length + 1, type: 'file' })"
-                >
-                  <q-item-section>File</q-item-section>
-                </q-item>
-              </q-list>
-            </q-menu>
-          </q-btn>
-        </div>
-      </div>
-    </div>
+      </q-card-section>
+    </q-card>
+
     <!-- Response Section-->
     <q-card class="q-mt-md" v-if="responseData">
-      <q-card-section>
+      <q-card-section class="q-py-sm">
         <div class="text-h6">Response</div>
       </q-card-section>
       <q-separator />
@@ -112,12 +124,13 @@
         </span> <span class="grey-8">{{ rawResponse.statusText }}</span>
       </q-card-section>
       <!-- Response Headers-->
-      <q-card-section>
+      <q-card-section class="q-py-sm">
         <q-expansion-item
+          dense
           v-model="headerExpanded"
         >
           <template v-slot:header>
-            <div class="text-h6">Headers</div>
+            <div class="text-h6 q-pl-none">Headers</div>
           </template>
           <div class="bg-grey-2 rounded-borders q-pa-md">
             <p v-for="(val, key, index) in rawResponse.headers" :key="index">
@@ -126,9 +139,9 @@
           </div>
         </q-expansion-item>
       </q-card-section>
-      <q-card-section style="overflow-x: scroll;">
+      <q-card-section class="q-py-sm">
         <pre
-          class="bg-grey-2 rounded-borders q-pa-sm shadow-1"
+          class="bg-grey-2 rounded-borders q-pa-sm scroll"
         ><code>{{ responseData }}</code></pre>
       </q-card-section>
     </q-card>
